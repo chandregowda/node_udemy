0607edf61bba45ccb615b56a5299eaf70e10cee9
chandregowda <chandregowda@gmail.com>
2017-10-01 18:59:12 +0530

    Added new handle bar function which accepts parameter and commeted in app.js

--- app.js
+++ app.js
@@ -2,55 +2,86 @@ const express = require("express");
 const hbs = require("hbs");
 const fs = require('fs');
 
-hbs.registerPartials(__dirname + "/views/partials/");
-hbs.registerHelper('getCurrentYear', () => new Date().getFullYear() );
-
-var app = express();
-app.set('view engine', 'hbs'); // Set the Template engine
-
-
-app.use((req, res) => {
-    res.render("maintenance.hbs");
-});
-app.use(express.static(__dirname + '/public'));
-
+/** 
+ * Consts used in application
+ */
 const PORT = 3000;
+const LOG_FILE = "server.log";
+const LOG_FILE_PATH = __dirname + "/" + LOG_FILE;
+
+/**
+ * Configure the path of Handlebars Partial files that are used multiple times
+ */
+hbs.registerPartials(__dirname + "/views/partials/");
+/**
+ * Register new Handlebar function getCurrentYear that will return current Year used in Footer section
+ */
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+/**
+ * Register new Handlebar function ScreamIt that will return passed string in uppercase
+ */
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+/**
+ * Create an Express App variable
+ */
+var app = express();
+/**
+ * Set up the Templating Engine - in this case Handlebars
+ */
+app.set('view engine', 'hbs');
+
+/**
+ * Using 'app.use' function we can add any number of middlewares
+ * NOTE: The sequence of this app.use matters
+ */
+
+// app.use((req, res) => {
+//     res.render("maintenance.hbs");
+// });
+
+/**
+ * Setup the Static directory from where CSS, Fonts, Images and Javascript or any html files are served
+ */
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
 
     var now = new Date().toString();
     let log = `New Reqest at  ${now}, ${req.method} ${req.url}`;
     console.log(log);
-    fs.appendFile("./server.log", log + "\n" , (err) => {
-        if(err) {
-            console.log("Unable to append ", log, 'to server.log');
+
+    fs.appendFile(LOG_FILE_PATH, log + "\n", (err) => {
+        if (err) {
+            console.log("Unable to append ", log, 'to', LOG_FILE_PATH);
         }
     })
-    next();
-
+    next(); // Without this next(), none of the below requests will be served.
 });
 
 app.get('/', (req, res) => {
     // res.json([{message:"Welcome", error:null}, {message:"Welcome 2", error:null}]);
     // res.send("Hi");
     res.render("home.hbs", {
-        pageTitle: "Welcome to my website",
-        currentYear : (new Date()).getFullYear()        
-    })
-});
-app.get('/about', (req, res)=> {
-    res.render('about.hbs', {
-        pageTitle: "About me",
-        currentYear : (new Date()).getFullYear()
+        pageTitle: "Welcome to my website"
     });
 });
 
-app.get('/bad', (req, res) => { 
-    res.json({error:"something bad happened", message:null});
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: "About me",
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.json({ error: "something bad happened", message: null });
 })
 
 app.get('*', (req, res) => {
     res.send("<h1>Omg No one to handle me</h1>");
 });
 
-app.listen(PORT, () => console.log("Server is listining at port ", PORT) );
+/**
+ * Start Server and Listen to PORT
+ */
+app.listen(PORT, () => console.log("Server is listining at port ", PORT));
